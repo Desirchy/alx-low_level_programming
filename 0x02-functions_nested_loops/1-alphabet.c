d4f88250abae52408b51e0a09544d6e0c2cf130c
Desirchy <evangelistgift4christ@gmail.com>
2022-09-22 07:13:39 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,14 @@
  * in lowecase followed by a new line
  * Return: void
  */
-
 void print_alphabet(void)
 {
 		char letter = 'a';
 
 		while (letter <= 'z')
 		{
-	        	_putchar(letter);
-            		letter++;
+			_putchar(letter);
+			letter++;
 		}
-			
 			_putchar('\n');
 }
