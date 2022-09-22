7263c281cd95367a88052ce96025db88d9d972ee
Desirchy <evangelistgift4christ@gmail.com>
2022-09-22 08:09:46 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
- *  * print_alphabet_x10 - a function that prints 10 times the alphabet
- *
- *   *
- *
- *    * Return: x10 a-z
- *
- *     */
+ * Return: x10 a-z
+ */
 
 void print_alphabet_x10(void)
 
@@ -16,28 +12,17 @@ void print_alphabet_x10(void)
 
 		int n, co;
 
+		co = 0;
 
+		while (co < 10)
+		{
+			for (n = 'a'; n <= 'z'; n++)
+			{
 
-			co = 0;
+			}
+			co++;
+			_putchar('\n');
 
-
-
-				while (co < 10)
-
-						{
-
-									for (n = 'a'; n <= 'z'; n++)
-
-												{
-
-																_putchar(n);
-
-																		}
-
-											co++;
-
-													_putchar('\n');
-
-														}
+								
 
 }
