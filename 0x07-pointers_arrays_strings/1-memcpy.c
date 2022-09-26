7c1d6e618b4d572a48299a5b0941ec6df9c29177
Desirchy <evangelistgift4christ@gmail.com>
2022-09-26 15:42:51 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,13 @@
-#include "main.h"                                                                                                                             
-
-/**                                                                                                                                           
- *
- *  *_memset - fills memory with a constant byte.                                                                                                
- *
- *   * @s: source string                                                                                                                          
- *
- *    * @b: the constant byte for filling                                                                                                          
- *
- *     * @n: length of buffer                                                                                                                       
- *
- *      * Return: new string                                                                                                                         
- *
- *       */                                                                                                                                                                                                                 
-
+#include "main.h"                                                                                                            
+/**           
+ *_memset - fills memory with a constant byte.                                   * @s: source string                                                        
+ * @b: the constant byte for filling
+ * @n: length of buffer
+ * * Return: new string           
+ */                                                                     
 char *_memcpy(char *dest, char *src, unsigned int n);                                                                                                
 {                                                                                                                                             
-	                unsigned int i = 0;
+                 	unsigned int i = 0;
 
 			while (i < n)
 			{
