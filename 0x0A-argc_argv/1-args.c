bf59c03fb8e434b474e2473670307144c13f10eb
Desirchy <evangelistgift4christ@gmail.com>
2022-09-30 11:32:32 -0700

    Task 5

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,12 +14,10 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		printf("%d\n", argc - 1);
 	else
-	{	
+	{
 		for (i = 0; *argv; i++, argv++);
-
+		
 		printf("%d\n", i - 1);
 	}
-
 	return (0);
 }
-
